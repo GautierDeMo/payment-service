bdfc29cb119c6ea53128c1a8d72075a7dacb3589
SolwenG <solene.gouin@orange.fr>
2026-05-12 12:11:21 +0200

    init project

--- app.js
+++ app.js
@@ -1,26 +1,40 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+require('dotenv').config()
 
-var app = express();
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  limit: 5, // 5 requests max
+  standardHeaders: true, // RateLimit-* headers
+  legacyHeaders: false, // X-RateLimit-* headers
+  message: {
+    error: 'Too many requests, please try again later.',
+  },
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(helmet());
+app.use(limiter);
+app.use(logger('dev'));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
